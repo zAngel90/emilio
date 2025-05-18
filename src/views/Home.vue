1e9149484f179ca27dce9bf4484e1cc51fb00152
zAngel90 <zangel90@gmail.com>
2025-05-17 19:51:39 -0500

    primer commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,10 @@ import Services from '../components/Services.vue'
 import About from '../components/About.vue'
 import Contact from '../components/Contact.vue'
 import HomeGallery from '../components/HomeGallery.vue'
-import PaymentOverlay from '../components/PaymentOverlay.vue'
 </script>
 
 <template>
   <div class="home">
-    <PaymentOverlay />
     <Hero />
     <HomeGallery />
     <Services />
